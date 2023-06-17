82f2c2502a8427ba4d5ca45f9f555bd6a5dcf000
Jinyu Chen <rzco0717@gmail.com>
2023-06-17 00:02:59 -0400

    add fake data & generate files

--- data/clean.py
+++ data/clean.py
@@ -35,38 +35,51 @@ def main(args):
         if file.startswith('~$'): 
             print(f'{color.RED}> File {file} is a temporary file generated from Excel, skipping ... {color.END}')
             continue
-        clean(file, args.output_format)
+        clean(file, args.output_format, args.final)
         print()
 
-def clean(filename: str, format: str):
+def clean(filename: str, format: str, onlyOutputFinalFile: bool):
     filename_without_extension = os.path.splitext(filename)[0]
     def output(suffix: str):
         is_csv = format == "csv"
         fun = df.to_csv if is_csv else df.to_excel
-        fun(f'{filename_without_extension}_{suffix}.{"csv" if is_csv else "xlsx"}', index=False)
+        if onlyOutputFinalFile:
+            if "fake" in suffix:
+                fun(f'{filename_without_extension}_{suffix}.{"csv" if is_csv else "xlsx"}', index=False)
+        else:
+            fun(f'{filename_without_extension}_{suffix}.{"csv" if is_csv else "xlsx"}', index=False)
+
 
     df = pd.read_excel(filename)
-    df.columns = ['title','question','doc1','info1','ans1','doc2','info2','ans2','doc3','info3','ans3']
-
-    replace_keywords = [
-        '\n', '\t', '\xa0', 
-        '健康咨询描述：',
-        '病情分析：',
-        '指导意见：',
-        '处理意见：',
-        '^\s+|\s+$', # 去掉 leading and trailing whitespaces
-    ]
-    df = df.replace(
-        { keyword: '' for keyword in replace_keywords },
-        regex=True
-    )
-    df[['doc1', 'hosp1', 'pos1']] = df['doc1'].str.split(' ', n=2, expand=True)
-    df[['doc2', 'hosp2', 'pos2']] = df['doc2'].str.split(' ', n=2, expand=True)
-    df[['doc3', 'hosp3', 'pos3']] = df['doc3'].str.split(' ', n=2, expand=True)
-    df = df[['title','question','doc1','hosp1','pos1','info1','ans1','doc2','hosp2','pos2','info2','ans2','doc3','hosp3','pos3','info3','ans3']]
-    output(suffix="replaced")
+    is_processed = False
+    if df.columns.size == 18:
+        print(f'{color.RED}> This file has been processed by this program, skipping some steps ... {color.END}', end='\n')
+        is_processed = True
+        df.columns = ['isExcluded', 'title','question','doc1','hosp1','pos1','info1',
+                      'ans1','doc2','hosp2','pos2','info2','ans2','doc3',
+                      'hosp3','pos3','info3','ans3']
+    else:
+        df.columns = ['title','question','doc1','info1','ans1','doc2','info2','ans2','doc3','info3','ans3']
+        replace_keywords = [
+            '\n', '\t', '\xa0', 
+            '健康咨询描述：',
+            '病情分析：',
+            '指导意见：',
+            '处理意见：',
+            '^\s+|\s+$', # 去掉 leading and trailing whitespaces
+        ]
+        df = df.replace(
+            { keyword: '' for keyword in replace_keywords },
+            regex=True
+        )
+        df[['doc1', 'hosp1', 'pos1']] = df['doc1'].str.split(' ', n=2, expand=True)
+        df[['doc2', 'hosp2', 'pos2']] = df['doc2'].str.split(' ', n=2, expand=True)
+        df[['doc3', 'hosp3', 'pos3']] = df['doc3'].str.split(' ', n=2, expand=True)
+        df = df[['title','question','doc1','hosp1','pos1','info1','ans1','doc2','hosp2','pos2','info2','ans2','doc3','hosp3','pos3','info3','ans3']]
+        output(suffix="replaced")
 
     exclusion_keywords = [
+        '*',
         '图',
         '检查结果',
     ]
@@ -78,7 +91,10 @@ def clean(filename: str, format: str):
     df = df[~mask]
     excluded_by_keywords = sorted(list(set(original) - set(df.index)))
     original = df.index.tolist()
-    df = df.dropna()
+    if is_processed: 
+        df = df.drop(df.columns[0], axis=1)
+    else:
+        df = df.dropna()
     excluded_by_na_cells = sorted(list(set(original) - set(df.index)))
     
     num_exlusions_by_keywords = len(excluded_by_keywords)
@@ -88,16 +104,27 @@ def clean(filename: str, format: str):
         if i > 0 and i % 10 == 0: print(end='\n\t')
         if index == excluded_by_keywords[-1]: print(f'#{index}')
         else: print(f'#{index}, ', end='')
-    print(f'{color.BLUE}> Excluded indices (by empty cell, in total of {num_exlusions_by_na_cells} lines): {color.END}', end='\n\t')
-    for (i, index) in enumerate(excluded_by_na_cells):
-        if i > 0 and i % 10 == 0: print(end='\n\t')
-        if index == excluded_by_na_cells[-1]: print(f'#{index}')
-        else: print(f'#{index}, ', end='')
+    if not is_processed:
+        print(f'{color.BLUE}> Excluded indices (by empty cell, in total of {num_exlusions_by_na_cells} lines): {color.END}', end='\n\t')
+        for (i, index) in enumerate(excluded_by_na_cells):
+            if i > 0 and i % 10 == 0: print(end='\n\t')
+            if index == excluded_by_na_cells[-1]: print(f'#{index}')
+            else: print(f'#{index}, ', end='')
     print(f">>> {color.UNDERLINE}Total of {num_exlusions_by_keywords + num_exlusions_by_na_cells} lines are excluded{color.END} <<<")
     output(suffix="excluded")
 
     df = df.head(100)
     output(suffix="excluded_100")
+
+    fake_data = [
+        (39, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为4。','','','','','','','','','','','','','','','']),
+        (59, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为2。','','','','','','','','','','','','','','','']),
+        (79, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为1。','','','','','','','','','','','','','','','']),
+    ]
+    indices = [ df.index[i] for (i, _) in fake_data ]
+    for (i, (loc, fake_ques)) in enumerate(fake_data):
+        df.loc[indices[i]] = fake_ques
+    output(suffix='excluded_100_fake')
 
 if __name__ == '__main__':
     import os
@@ -125,6 +152,11 @@ if __name__ == '__main__':
         "-o", "--output-format",
         choices=["csv", "excel"],
         default="csv",
+        help="the output format of cleaned up file(s)"
+    )
+    parser.add_argument(
+        "-f", "--final",
+        action="store_true",
         help="the output format of cleaned up file(s)"
     )
 
