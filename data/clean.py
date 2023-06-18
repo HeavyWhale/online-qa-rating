af3aa3a89cc6fc113e82b581b8f7ec57cd1eaec4
Jinyu Chen <rzco0717@gmail.com>
2023-06-18 17:29:30 -0400

    make `-f` only output single final file

--- data/clean.py
+++ data/clean.py
@@ -37,6 +37,19 @@ def main(args):
             continue
         clean(file, args.output_format, args.final)
         print()
+    
+    if args.final:
+        print(f'{color.YELLOW}> Combining all generated *_FINAL.csv files ...{color.END}')
+        files = list(filter(lambda f: f.find('FINAL') != -1, os.listdir(os.getcwd())))
+        dfs = []
+        for file in files:
+            dfs.append(pd.read_csv(file))
+            os.system(f'rm \'{file}\'')
+        combined_df = pd.concat(dfs, ignore_index=True)
+        fun = combined_df.to_csv if args.output_format == 'csv' else combined_df.to_excel
+        fun('final.csv', header=False, index=False)
+        print(f'{color.BLUE}> Finished combining!')
+
 
 def clean(filename: str, format: str, onlyOutputFinalFile: bool):
     filename_without_extension = os.path.splitext(filename)[0]
@@ -44,11 +57,10 @@ def clean(filename: str, format: str, onlyOutputFinalFile: bool):
         is_csv = format == "csv"
         fun = df.to_csv if is_csv else df.to_excel
         if onlyOutputFinalFile:
-            if "fake" in suffix:
-                fun(f'{filename_without_extension}_{suffix}.{"csv" if is_csv else "xlsx"}', index=False)
+            if "FINAL" in suffix:
+                fun(f'{filename_without_extension}_{suffix}.csv', index=False)
         else:
             fun(f'{filename_without_extension}_{suffix}.{"csv" if is_csv else "xlsx"}', index=False)
-
 
     df = pd.read_excel(filename)
     is_processed = False
@@ -58,6 +70,8 @@ def clean(filename: str, format: str, onlyOutputFinalFile: bool):
         df.columns = ['isExcluded', 'title','description','doc1','hosp1','pos1','info1',
                       'ans1','doc2','hosp2','pos2','info2','ans2','doc3',
                       'hosp3','pos3','info3','ans3']
+        df['category'] = filename_without_extension
+        df = pd.DataFrame(df['category']).join(df.drop('category', axis=1))
     else:
         df.columns = ['title','description','doc1','info1','ans1','doc2','info2','ans2','doc3','info3','ans3']
         replace_keywords = [
@@ -76,6 +90,8 @@ def clean(filename: str, format: str, onlyOutputFinalFile: bool):
         df[['doc2', 'hosp2', 'pos2']] = df['doc2'].str.split(' ', n=2, expand=True)
         df[['doc3', 'hosp3', 'pos3']] = df['doc3'].str.split(' ', n=2, expand=True)
         df = df[['title','description','doc1','hosp1','pos1','info1','ans1','doc2','hosp2','pos2','info2','ans2','doc3','hosp3','pos3','info3','ans3']]
+        df['category'] = '_'.join(filename_without_extension.split())
+        df = pd.DataFrame(df['category']).join(df.drop('category', axis=1))
         output(suffix="replaced")
 
     exclusion_keywords = [
@@ -92,7 +108,7 @@ def clean(filename: str, format: str, onlyOutputFinalFile: bool):
     excluded_by_keywords = sorted(list(set(original) - set(df.index)))
     original = df.index.tolist()
     if is_processed: 
-        df = df.drop(df.columns[0], axis=1)
+        df = df.drop(df.columns[1], axis=1)
     else:
         df = df.dropna()
     excluded_by_na_cells = sorted(list(set(original) - set(df.index)))
@@ -110,21 +126,21 @@ def clean(filename: str, format: str, onlyOutputFinalFile: bool):
             if i > 0 and i % 10 == 0: print(end='\n\t')
             if index == excluded_by_na_cells[-1]: print(f'#{index}')
             else: print(f'#{index}, ', end='')
-    print(f">>> {color.UNDERLINE}Total of {num_exlusions_by_keywords + num_exlusions_by_na_cells} lines are excluded{color.END} <<<")
+    print(f">>> {color.UNDERLINE}Total of {num_exlusions_by_keywords + num_exlusions_by_na_cells} lines are excluded, {len(df)} lines are left{color.END} <<<")
     output(suffix="excluded")
 
     df = df.head(100)
     output(suffix="excluded_100")
 
     fake_data = [
-        (39, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为4。','','','','','','','','','','','','','','','']),
-        (59, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为2。','','','','','','','','','','','','','','','']),
-        (79, ['注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为1。','','','','','','','','','','','','','','','']),
+        (39, ['_'.join(filename_without_extension.split()), '注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为4。','','','','','','','','','','','','','','','']),
+        (59, ['_'.join(filename_without_extension.split()), '注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为2。','','','','','','','','','','','','','','','']),
+        (79, ['_'.join(filename_without_extension.split()), '注意力问题','如果您看到此问题，请将所有选项（诊断评分、疾病熟悉程度）选择为1。','','','','','','','','','','','','','','','']),
     ]
     indices = [ df.index[i] for (i, _) in fake_data ]
     for (i, (loc, fake_ques)) in enumerate(fake_data):
         df.loc[indices[i]] = fake_ques
-    output(suffix='excluded_100_fake')
+    output(suffix='FINAL')
 
 if __name__ == '__main__':
     import os
@@ -157,7 +173,7 @@ if __name__ == '__main__':
     parser.add_argument(
         "-f", "--final",
         action="store_true",
-        help="only output the final clean file"
+        help="only output the single final clean file"
     )
 
     main(parser.parse_args())
